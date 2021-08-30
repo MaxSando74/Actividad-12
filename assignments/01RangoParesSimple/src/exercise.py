700f77112dacd80bb1b0ecdacdf962302b335d03
MaxSando74 <89033313+MaxSando74@users.noreply.github.com>
2021-08-30 11:30:38 -0600

    Update exercise.py

--- assignments/01RangoParesSimple/src/exercise.py
+++ assignments/01RangoParesSimple/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea    
-    pass
+    x= int(input())
+    y= int(input())
+        while x<=y:
+            if (x%2) == 0:
+                print(x)
+            x+=1
 
 if __name__=='__main__':
     main()
